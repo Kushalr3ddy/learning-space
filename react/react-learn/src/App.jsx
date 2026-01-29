3b56279649f7b42d45579bdd0d45159fae6e9578
kushalr3ddy <46622551+Kushalr3ddy@users.noreply.github.com>
2026-01-29 15:28:53 +0000

    props and stuff

--- react/react-learn/src/App.jsx
+++ react/react-learn/src/App.jsx
@@ -2,7 +2,13 @@ import Student from './Student.jsx';
 
 function App() {
     return(
-        <Student name="spongebob"/>
+        <>
+        <Student name="spongebob" age={30} isStudent={true}/>
+        <Student name="patrick" age={42} isStudent={false}/>
+        <Student name="Squidward" age={50} isStudent={false} />
+        <Student name="sandy" age={27} isStudent={true} />
+        <Student name="Larry"/>
+        </>
     );
 }
 
