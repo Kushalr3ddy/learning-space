d9e3b2f4fc0904fb287283ca0f862417e47e74e1
kushalr3ddy <46622551+Kushalr3ddy@users.noreply.github.com>
2026-02-10 19:16:28 +0000

    button: simple button component

--- react/react-learn/src/App.jsx
+++ react/react-learn/src/App.jsx
@@ -1,28 +1,12 @@
-import List from "./List";
+import Button from './Button.jsx';
 
 function App() {
-    const fruits = [
-        // giving each element a unique id cause two element can have same .name or same prop values
-        {id:1,name:'Apple', calories:95}, 
-        {id:2,name:'Orange', calories:45},
-        {id:3,name:'Banana', calories:105},
-        {id:4,name:'coconut', calories:159},
-        {id:5,name:'pineapple', calories:37},
-    ];
-
-    const vegetables = [
-        // giving each element a unique id cause two element can have same .name or same prop values
-        {id:6,name:'potatoes', calories:110}, 
-        {id:7,name:'celery', calories:15},
-        {id:8,name:'carrots', calories:25},
-        {id:9,name:'corn', calories:63},
-        {id:10,name:'broccoli', calories:58},
-    ];
+    
+    
 
     return(
     <>
-        {fruits.length > 0 ? <List items={fruits} category="fruits"/>:null}
-        {vegetables.length > 0 ?<List items={vegetables} category="vegetables"/>: null}
+        <Button/>
     </>
     );
 }
--- react/react-learn/src/Button.jsx
+++ react/react-learn/src/Button.jsx
@@ -2,17 +2,29 @@
 
 function Button() {
     
-    const styles = {    
-        backgroundColor: "hsl(200, 100%, 50%)",
-        color: "white",
-        padding: "10px 20px",
-        borderRadius: "5px",
-        border: "none",
-        cursor: "pointer",
+    // const styles = {    
+    //     backgroundColor: "hsl(200, 100%, 50%)",
+    //     color: "white",
+    //     padding: "10px 20px",
+    //     borderRadius: "5px",
+    //     border: "none",
+    //     cursor: "pointer",
+    // }
+    let count =0;
+    const handleClick = (name) => {
+        if(count<3){
+            count++;
+            console.log(`${name} you clicked me ${count} times/s`);
+        }else{
+            console.log(`${name} stop clicking me`);
+        }
     }
 
+    const handleClick2 =(name) => console.log(`${name} stop clicking me`);
+
     return(
-        <button style={styles}>click me</button>
+        <button onClick={()=>handleClick("sed")}>click me</button>
+        // <button onClick={()=>{handleClick2("sed")}}>click me</button>
     );
 }
 
