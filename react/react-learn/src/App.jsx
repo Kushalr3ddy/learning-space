56bce1d1403e0ba3dbdd08ac0cfb7f09b15300ad
kushalr3ddy <46622551+Kushalr3ddy@users.noreply.github.com>
2026-04-18 17:24:24 +0000

    updating array of objects

--- react/react-learn/src/App.jsx
+++ react/react-learn/src/App.jsx
@@ -1,12 +1,12 @@
-import MyComponent from "./MyComponent.jsx";
+import { useState } from "react";
+import MyObjectArrayComponent from "./MyObjectArrayComponent.jsx";
 
 
 function App() {
     
     
-
     return(
-        <MyComponent />
+        <MyObjectArrayComponent />
     );
 }
 
